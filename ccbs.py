cca200d7e690e85046e237660d473ac5fe4fefbf
Yehor Merzlov <miozzik@gmail.com>
2020-05-03 01:57:50 +0300

    Создаём папку

--- ccbs.py
+++ ccbs.py
@@ -1,5 +1,13 @@
-import paramiko
-import getpass
-import argparse
 import os
-import time
+from datetime import date
+
+# Создаём директорию для бэкапа
+def create_folder():
+    if os.path.isdir("Backup"):
+        pass
+    else:
+        os.mkdir("Backup")
+    os.chdir("Backup")
+    current_date = str(date.today())
+    os.mkdir(current_date)
+    return True
