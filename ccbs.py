a6e9c79592ae349d42a3a24f285768ee5a811a7c
Yehor Merzlov <miozzik@gmail.com>
2020-05-03 02:09:33 +0300

    Мелкие изменения

--- ccbs.py
+++ ccbs.py
@@ -2,12 +2,13 @@ import os
 from datetime import date
 
 # Создаём директорию для бэкапа
-def create_folder():
-    if os.path.isdir("Backup"):
-        pass
-    else:
-        os.mkdir("Backup")
-    os.chdir("Backup")
-    current_date = str(date.today())
-    os.mkdir(current_date)
-    return True
+
+if os.path.isdir("Backup"):
+    pass
+else:
+    os.mkdir("Backup")
+os.chdir("Backup")
+current_date = str(date.today())
+os.mkdir(current_date)
+
+# Переходим к файлу с адрессами устройств
