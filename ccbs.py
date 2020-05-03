b8780c1fd696684ae2b3be40895935e6dbe87ecd
Yehor Merzlov <miozzik@gmail.com>
2020-05-03 23:58:34 +0300

    Готова функция для создания папки

--- ccbs.py
+++ ccbs.py
@@ -1,14 +1,19 @@
 import os
+import re
+import paramiko
 from datetime import date
 
 # Создаём директорию для бэкапа
+def create_folder(dirname=None):
+    if not os.path.isdir("Backup"):
+        os.mkdir("Backup")
+    os.chdir("Backup")
+    current_date = str(date.today())
+    if dirname and not os.path.isdir(dirname):
+        os.mkdir(dirname)
+    elif dirname==None and not os.path.isdir(current_date):
+        os.mkdir(current_date)
+    os.chdir("..")
+    return print("Папка подготовлена")
 
-if os.path.isdir("Backup"):
-    pass
-else:
-    os.mkdir("Backup")
-os.chdir("Backup")
-current_date = str(date.today())
-os.mkdir(current_date)
-
-# Переходим к файлу с адрессами устройств
+create_folder()
