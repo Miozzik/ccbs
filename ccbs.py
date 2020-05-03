54491a4f6aa1417ef1caaf81da67277229b25d6a
Yehor Merzlov <miozzik@gmail.com>
2020-05-04 00:34:53 +0300

    Получаеи нужные данные

--- ccbs.py
+++ ccbs.py
@@ -16,4 +16,18 @@ def create_folder(dirname=None):
     os.chdir("..")
     return print("Папка подготовлена")
 
+
 create_folder()
+
+
+open_client_list = open("client_list.txt", encoding="utf-8").readlines()
+# Получаеи нужные данные
+for readoneline in open_client_list:
+    readoneline = readoneline.strip()
+    ip, port = re.search(r"(\d+.\d+.\d+.\d+|\S+[a-zA-Z]):*(\d*)", readoneline).group(1, 2)
+    device_name = re.search(r'(\d+.\d+.\d+.\d+|\S+[a-zA-Z]):*\d*.+"(.*)"', readoneline).group(2)
+    if not len(device_name):
+        device_name = "Устройство с НЕ заданым названием"
+    find_enable_password = re.search(r"enable: (\S*)", readoneline).group(1)
+    ssh_ip, ssh_password = re.search(r'.+\s(\w+):(\S+)', readoneline).group(1, 2)
+    
