7ad8553461085b8d13be24f2887e459e06a3e350
Yehor Merzlov <miozzik@gmail.com>
2020-05-04 03:43:48 +0300

    Почти готовенько

--- ccbs.py
+++ ccbs.py
@@ -1,33 +1,69 @@
 import os
 import re
 import paramiko
+import time
 from datetime import date
 
 # Создаём директорию для бэкапа
-def create_folder(dirname=None):
-    if not os.path.isdir("Backup"):
-        os.mkdir("Backup")
-    os.chdir("Backup")
-    current_date = str(date.today())
-    if dirname and not os.path.isdir(dirname):
-        os.mkdir(dirname)
-    elif dirname==None and not os.path.isdir(current_date):
-        os.mkdir(current_date)
-    os.chdir("..")
-    return print("Папка подготовлена")
 
+if not os.path.isdir("Backup"):
+    os.mkdir("Backup")
+os.chdir("Backup")
+current_date = str(date.today())
+if not os.path.isdir(current_date):
+    os.mkdir(current_date)
 
-create_folder()
+os.chdir("..")
 
-
+temp_file = open(".temp_ccbs", "w", encoding = "utf-8")
 open_client_list = open("client_list.txt", encoding="utf-8").readlines()
-# Получаеи нужные данные
 for readoneline in open_client_list:
     readoneline = readoneline.strip()
-    ip, port = re.search(r"(\d+.\d+.\d+.\d+|\S+[a-zA-Z]):*(\d*)", readoneline).group(1, 2)
+    address, port = re.search(r"(\d+.\d+.\d+.\d+|\S+[a-zA-Z]):*(\d*)", readoneline).group(1, 2)
     device_name = re.search(r'(\d+.\d+.\d+.\d+|\S+[a-zA-Z]):*\d*.+"(.*)"', readoneline).group(2)
     if not len(device_name):
         device_name = "Устройство с НЕ заданым названием"
-    find_enable_password = re.search(r"enable: (\S*)", readoneline).group(1)
-    ssh_ip, ssh_password = re.search(r'.+\s(\w+):(\S+)', readoneline).group(1, 2)
-    
+    enable_password = re.search(r"enable: (\S*)", readoneline).group(1)
+    ssh_user, ssh_password = re.search(r'.+\s(\w+):(\S+)', readoneline).group(1, 2)
+    print(f"Подключаемся к: {device_name}")
+    client = paramiko.SSHClient() 
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+    client.connect(
+        hostname=address,
+        username=ssh_user,
+        password=ssh_password,
+        look_for_keys=False,
+        allow_agent=False)
+    with client.invoke_shell() as ssh:
+        ssh.send('enable\n')
+        ssh.send(enable_password + '\n')
+        time.sleep(0.5)
+        ssh.send('terminal length 0\n')
+        time.sleep(0.5)
+        ssh.recv(1000)
+        ssh.send('show startup-config\n')
+        time.sleep(0.5)
+        result = ssh.recv(10000).decode('utf-8')
+    temp_file.write(result)
+    temp_file.close()
+
+    with open(".temp_ccbs", encoding="utf-8") as temp:
+        os.chdir("Backup")
+        os.chdir(current_date)
+        temp = temp.readlines()
+        #hostname = ""
+        for i in temp:
+            if not i.startswith("!") and not i.startswith("\n"):
+                if i.startswith("hostname"):
+                    hostname = (re.search(r"hostname (\S*)", i).group(1))
+        with open(hostname, "w+", encoding="utf-8") as result_file:
+            for i in temp[1:-1]:
+                if not i.startswith("!") and not i.startswith("\n"):
+                    if i == "end":
+                        break
+                    result_file.write(i)
+
+    print(f"Устройство {hostname} готово")
+    os.chdir("..")
+
+#print("Готово :)")
