6526c09226f2f3ed07c85535378ff2f5cf751537
Yehor Merzlov <miozzik@gmail.com>
2020-05-05 16:08:01 +0300

    Возможность ввода общих данных

--- ccbs.py
+++ ccbs.py
@@ -3,10 +3,10 @@ import re
 import paramiko
 import time
 import getpass
+import tempfile
 from datetime import date
 
 
-# Создаём директорию для бэкапа
 if not os.path.isdir("Backup"):
     os.mkdir("Backup")
 os.chdir("Backup")
@@ -15,18 +15,28 @@ if not os.path.isdir(current_date):
     os.mkdir(current_date)
 os.chdir("..")
 
-temp_file = open(".temp_ccbs", "w", encoding="utf-8")
-
 open_client_list = open("client_list.txt", encoding="utf-8").readlines()
+
+if len(open_client_list) < 22:
+    ssh_user = input("Логин SSH: ")
+    ssh_password = getpass.getpass("Пароль SSH: ")
+    enable_password = getpass.getpass("Пароль Пароль для enable: ")
+
+
 for readoneline in open_client_list:
     readoneline = readoneline.strip()
-    address, port = re.search(r"(\d+.\d+.\d+.\d+|\S+[a-zA-Z]):*(\d*)", readoneline).group(1, 2)
-    device_name = re.search(r'(\d+.\d+.\d+.\d+|\S+[a-zA-Z]):*\d*.+"(.*)"', readoneline).group(2)
-    if not len(device_name):
-        device_name = "Устройство с НЕ заданым названием"
-    enable_password = re.search(r"enable: (\S*)", readoneline).group(1)
-    ssh_user, ssh_password = re.search(r'.+\s(\w+):(\S+)', readoneline).group(1, 2)
-    print(f"Подключаемся к: {device_name}")
+    if len(readoneline) > 21:
+        address, port = re.search(r"(\d+.\d+.\d+.\d+|\S+[a-zA-Z]):*(\d*)", readoneline).group(1, 2)
+        device_name = re.search(r'(\d+.\d+.\d+.\d+|\S+[a-zA-Z]):*\d*.+"(.*)"', readoneline).group(2)
+        if not len(device_name):
+            device_name = address
+        enable_password = re.search(r"enable: (\S*)", readoneline).group(1)
+        ssh_user, ssh_password = re.search(r'.+\s(\w+):(\S+)', readoneline).group(1, 2)
+        print(f"Подключаемся к: {device_name}")
+    else:
+        address, port = readoneline.split(":")
+        print(f"Подключаемся к: {address}")
+
 
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
@@ -46,25 +56,26 @@ for readoneline in open_client_list:
         ssh.send('show startup-config\n')
         time.sleep(0.5)
         result = ssh.recv(10000).decode('utf-8')
-    temp_file.write(result)
-    temp_file.close()
+    with tempfile.NamedTemporaryFile(mode='w+') as temp_file:
+        temp_file.write(result)
+        temp_file.seek(0)
 
-    with open(".temp_ccbs", encoding="utf-8") as temp:
         os.chdir("Backup")
         os.chdir(current_date)
 
-        hostname = (re.findall(r"hostname (\S*)", temp.read()).group(1))
+        hostname = re.findall(r"hostname (\S*)", temp_file.read())[0]
 
-        with open(hostname, "w+", encoding="utf-8") as result_file:
-            for i in temp[1:-1].readlines():
+        temp_file.seek(0)
+
+        with open(hostname, "w", encoding="utf-8") as result_file:
+            for i in temp_file.readlines()[1:-1]:
                 if not i.startswith("!") and not i.startswith("\n"):
-                    if i == "end":
-                        break
                     result_file.write(i)
 
-
-    print(f"Устройство {hostname} готово")
-    os.chdir("..")
-    os.chdir("..")
-os.remove(".temp_ccbs")
-print("Готово :)")
+        temp_file.write("")
+        temp_file.seek(0)
+        print(f"Устройство {hostname} готово")
+        os.chdir("..")
+        os.chdir("..")
+else:
+    print("Готово :)")
